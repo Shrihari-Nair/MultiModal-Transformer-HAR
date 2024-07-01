f961ffe08577b5ec5a9f91143c9ede274ae898c0
Shrihari-Nair <shriharinair@htic.iitm.ac.in>
2024-07-01 16:29:01 +0530

    trainer

---
+++ train.py
@@ -0,0 +1,30 @@
+import torch
+import numpy as np
+from Make_Dataset import Poses3d_Dataset
+import PreProcessing_ncrc
+from Models.model_crossview_fusion import ActRecogTransformer
+from Plotter.visualize import get_plot
+import pickle
+from asam import ASAM, SAM
+from timm.loss import LabelSmoothingCrossEntropy
+import os
+
+exp = 'myexp-1' #Assign an experiment id
+
+if not os.path.exists('exps/'+exp+'/'):
+    os.makedirs('exps/'+exp+'/')
+PATH='exps/'+exp+'/'
+
+#CUDA for PyTorch
+print("Using CUDA....")
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+torch.backends.cudnn.benchmark = True
+
+# Parameters
+print("Creating params....")
+params = {'batch_size':8,
+          'shuffle': True,
+          'num_workers': 3}
+max_epochs = 250
