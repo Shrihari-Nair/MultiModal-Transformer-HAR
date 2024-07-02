aeb4464443ded41fca88d3b1564e038611fa2332
Shrihari-Nair <shriharinair@htic.iitm.ac.in>
2024-07-02 15:19:24 +0530

    complete trainer

--- train.py
+++ train.py
@@ -1,11 +1,9 @@
 import torch
-import numpy as np
 from create_dataset import Poses3d_Dataset
-import PreProcessing_ncrc
-from Models.model_crossview_fusion import ActRecogTransformer
-from Plotter.visualize import get_plot
-import pickle
-from asam import ASAM, SAM
+import preprocessor
+from models.cross_view_fusion import ActRecogTransformer
+from plotter.visualizer import get_plot
+from asam import ASAM
 from timm.loss import LabelSmoothingCrossEntropy
 import os
 
@@ -31,7 +29,7 @@ params = {'batch_size':8,
           'num_workers': 3}
 max_epochs = 250
 
-pose2id, labels, partition = PreProcessing_ncrc.preprocess()
+pose2id, labels, partition = preprocessor.preprocess()
 
 print("Creating Data Generators...")
 mocap_frames = 600
@@ -48,3 +46,111 @@ validation_generator = torch.utils.data.DataLoader(validation_set, **params) #Ea
 print("Initiating Model...")
 model = ActRecogTransformer(device)
 model = model.to(device)
+
+# Define Loss Function and Optimizer
+lr=0.0025
+wt_decay=5e-4
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9,weight_decay=wt_decay)
+
+#ASAM
+rho=0.5
+eta=0.01
+minimizer = ASAM(optimizer, model, rho=rho, eta=eta)
+
+#Learning Rate Scheduler
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(minimizer.optimizer, max_epochs)
+print("Using cosine")
+
+#TRAINING AND VALIDATING
+epoch_loss_train=[]
+epoch_loss_val=[]
+epoch_acc_train=[]
+epoch_acc_val=[]
+
+#Label smoothing
+smoothing=0.1
+criterion = LabelSmoothingCrossEntropy(smoothing=smoothing)
+print("Loss: LSC ",smoothing)
+
+best_accuracy = 0.
+
+
+print("Begin Training....")
+for epoch in range(max_epochs):
+    # Train
+    model.train()
+    loss = 0.
+    accuracy = 0.
+    cnt = 0.
+    for inputs, targets in training_generator:
+        inputs = inputs.to(device); #print("Input batch: ",inputs)
+        targets = targets.to(device)
+
+        optimizer.zero_grad()
+
+        # Ascent Step
+        #print("labels: ",targets)
+        predictions = model(inputs.float())
+        #print("predictions: ",torch.argmax(predictions, 1) )
+        batch_loss = criterion(predictions, targets)
+        batch_loss.mean().backward()
+        minimizer.ascent_step()
+
+        # Descent Step
+        criterion(model(inputs.float()), targets).mean().backward()
+        minimizer.descent_step()
+
+        with torch.no_grad():
+            loss += batch_loss.sum().item()
+            accuracy += (torch.argmax(predictions, 1) == targets).sum().item()
+        cnt += len(targets)
+    loss /= cnt
+    accuracy *= 100. / cnt
+    print(f"Epoch: {epoch}, Train accuracy: {accuracy:6.2f} %, Train loss: {loss:8.5f}")
+    epoch_loss_train.append(loss)
+    epoch_acc_train.append(accuracy)
+    #scheduler.step()
+
+    #accuracy,loss = validation(model,validation_generator)
+    #Test
+    model.eval()
+    loss = 0.
+    accuracy = 0.
+    cnt = 0.
+    model=model.to(device)
+    with torch.no_grad():
+        for inputs, targets in validation_generator:
+
+            b = inputs.shape[0]
+            inputs = inputs.to(device); #print("Validation input: ",inputs)
+            targets = targets.to(device)
+            
+            predictions = model(inputs.float())
+            
+            with torch.no_grad():
+                loss += batch_loss.sum().item()
+                accuracy += (torch.argmax(predictions, 1) == targets).sum().item()
+            cnt += len(targets)
+        loss /= cnt
+        accuracy *= 100. / cnt
+        
+    
+        if best_accuracy < accuracy:
+            best_accuracy = accuracy
+            torch.save(model.state_dict(),PATH+exp+'_best_ckpt.pt')
+            print("Check point "+PATH+exp+'_best_ckpt.pt'+ ' Saved!')
+
+    print(f"Epoch: {epoch},Test accuracy:  {accuracy:6.2f} %, Test loss:  {loss:8.5f}")
+
+
+    epoch_loss_val.append(loss)
+    epoch_acc_val.append(accuracy)
+
+
+print(f"Best test accuracy: {best_accuracy}")
+print("TRAINING COMPLETED :)")
+
+#Save visualization
+get_plot(PATH,epoch_acc_train,epoch_acc_val,'Accuracy-'+exp,'Train Accuracy','Val Accuracy','Epochs','Acc')
+get_plot(PATH,epoch_loss_train,epoch_loss_val,'Loss-'+exp,'Train Loss','Val Loss','Epochs','Loss')
