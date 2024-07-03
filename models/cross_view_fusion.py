40bfe8baf82b93f357f74c4bdccdc9037de0b417
Shrihari-Nair <shriharinair@htic.iitm.ac.in>
2024-07-03 17:09:03 +0530

    cva

--- models/cross_view_fusion.py
+++ models/cross_view_fusion.py
@@ -15,7 +15,8 @@ from .model_utils import Block
 
 
 class Action_Recognition_Transformer(nn.Module):
-    def __init__(self, device='cpu' , mocap_frames=600, acc_frames=150, num_joints=29, in_chans=3, acc_coords=3, acc_features=18, spatial_embed=32,
+    def __init__(self, device='cpu' , mocap_frames=600, acc_frames=150, num_joints=29, 
+                 in_chans=3, acc_coords=3, acc_features=18, spatial_embed=32,
                  sdepth=4, adepth=4, tdepth=4, num_heads=8, mlp_ratio=2., qkv_bias=True,
                  qk_scale=None, op_type='cls', embed_type='lin', fuse_acc_features=False,
                  drop_rate=0., attn_drop_rate=0., drop_path_rate=0.2,  norm_layer=None, num_classes=6):
